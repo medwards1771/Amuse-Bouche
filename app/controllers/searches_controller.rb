73e677f13071f8bf89bc1f0edcf49c82b01f9493
Meredith Edwards <meredith.edwards1771@gmail.com>
2016-07-17 17:49:55 -0400

    Create job results in searches controller, not in search model

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,27 +1,38 @@
 class SearchesController < ApplicationController
-
-  def show
-    @search = Search.find(params[:id])
-    @jobs = @search.get_results
-  end
+  attr_reader :job_results
 
   def new
     @search = Search.new
     @companies = Company.all
   end
 
+  def show
+    @search = Search.find(params[:id])
+    @job_results = @search.job_results
+  end
+
   def create
-    @search = Search.new(search_params)
-    if @search.save
-      redirect_to search_path(@search)
-    else
-      render :new, error: 'The search was not saved.'
+    @search = Search.create(search_params)
+    job_results = Adapters::JobClient.new(search_params).results
+    job_results.map do |job|
+      JobResult.create(job_result_params(job, @search.id))
     end
+    redirect_to search_path(@search)
   end
 
   private
 
   def search_params
     params[:search].permit(categories: [], companies: [], levels: [], locations: [])
+  end
+
+  def job_result_params(job, search_id)
+    { uid: job['id'].to_i,
+      title: job['name'],
+      landing_page: job['refs']['landing_page'],
+      company_name: job['company']['name'],
+      company_id: job['company']['id'],
+      search_id: search_id
+    }
   end
 end
--- app/models/adapters/job_client.rb
+++ app/models/adapters/job_client.rb
@@ -1,19 +1,12 @@
 class Adapters::JobClient
-  attr_reader :search_params, :response, :page_count, :pages
+  attr_reader :search_params, :page_count, :response, :connection, :pages
 
   def initialize(search_params)
     @search_params = search_params
   end
 
-  def job_results
-    if page_count > 1
-      jobs = pages.map do |page|
-        connection.query('/jobs', page, params=query_params)['results']
-      end.flatten
-    else
-      jobs = response['results']
-    end
-    jobs
+  def results
+    response['results']
   end
 
   private
@@ -22,17 +15,16 @@ class Adapters::JobClient
      @response ||= connection.query('/jobs', 0, params=query_params)
   end
 
-  def page_count
-    @page_count ||= response['page_count']
+  def connection
+    @connection ||= Adapters::MuseConnection.new
   end
 
   def pages
-    @pages ||= Array(0..9)
-    # @pages ||= Array(0..(page_count - 1))
+    @pages ||= Array(0..(page_count - 1))
   end
 
-  def connection
-    @connection ||= Adapters::MuseConnection.new
+  def page_count
+    @page_count ||= response['page_count']
   end
 
   def query_params
@@ -41,26 +33,26 @@ class Adapters::JobClient
   end
 
   def company_params
-    companies = search_params[:companies]
+    companies = search_params[:companies] || []
     companies = replace_spaces(companies) if companies.select{ |company| company.include?(" ") }.present?
     companies.present? ? 'company=' + companies.join('&company=') : ""
   end
 
   def category_params
-    categories = search_params[:categories]
+    categories = search_params[:categories] || []
     categories = replace_ampersands(categories) if categories.select{ |category| category.include?("&") }.present?
     categories = replace_spaces(categories) if categories.select{ |category| category.include?(" ") }.present?
     categories.present? ? 'category=' + categories.join("&category=") : ""
   end
 
   def level_params
-    levels = search_params[:levels]
+    levels = search_params[:levels] || []
     levels = replace_spaces(levels) if levels.select{ |level| level.include?(" ") }.present?
     levels.present? ? 'level=' + levels.join("&level=") : ""
   end
 
   def location_params
-    locations = search_params[:locations]
+    locations = search_params[:locations] || []
     locations = replace_commas(locations) if locations.select{ |location| location.include?(",") }.present?
     locations = replace_spaces(locations) if locations.select{ |location| location.include?(" ") }.present?
     locations.present? ? 'location=' + locations.join("&location=") : ""
